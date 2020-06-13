d1f66d773100bb876369086ca70ab5010fd8b277
amadeuszsz <amadeuszszymko@gmail.com>
2020-06-13 21:51:37 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	main.py
    #	traffic_sign_recognition.py

--- main.py
+++ main.py
@@ -63,11 +63,15 @@ def main():
         #Do things here
         #x = LaneDetection(frame)
         #name, final_frame = x.lane_detection()
-        signRecognizer = TrafficSignRecognition(frame)
+
+
+        #signRecognizer = TrafficSignRecognition(frame)
         name = "Traffic Sign Recognition"
         final_frame = TrafficSignRecognition(frame).connected_components()
-        final_frame = signRecognizer.frame_preprocessing()
-        classification = signRecognizer.templateSumSquare()
+        #final_frame = signRecognizer.frame_preprocessing()
+        #classification = signRecognizer.templateSumSquare()
+
+
         #######################################
         cv2.imshow(name, final_frame)
         key = cv2.waitKey(1)
