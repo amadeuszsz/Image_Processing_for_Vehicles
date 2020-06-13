67c58e11a210f54e6fd5e5304f2ab86d4b07f65f
amadeuszsz <amadeuszszymko@gmail.com>
2020-06-13 13:41:28 +0100

    Commit

--- traffic_sign_recognition.py
+++ traffic_sign_recognition.py
@@ -8,11 +8,12 @@ class TrafficSignRecognition():
     def frame_preprocessing(self):
         self.hsv = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
         # define range of blue color in HSV
-        self.lower_blue = np.array([165, 0, 0])
-        self.upper_blue = np.array([195, 255, 255])
-        # Threshold the HSV image to get only blue colors
-        self.mask = cv2.inRange(self.hsv, self.lower_blue, self.upper_blue)
-        # Bitwise-AND mask and original image
-        self.res = cv2.bitwise_and(self.frame, self.frame, mask=self.mask)
 
-        return self.res
+        lower_blue = np.array([0, 0, 0])
+        upper_blue = np.array([255, 255, 255])
+        # Threshold the HSV image to get only blue colors
+        mask = cv2.inRange(self.hsv, lower_blue, upper_blue)
+        # Bitwise-AND mask and original image
+        res = cv2.bitwise_and(self.frame, self.frame, mask=mask)
+
+        return res
