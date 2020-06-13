d51144fb3546820f0a343edcf05b96ac089fd577
Szymani <Szymanski996@gmail.com>
2020-06-13 13:38:34 +0100

    added sign class plus minor changes

---
+++ sign.py
@@ -0,0 +1,8 @@
+
+
+class Sign():
+    width = None
+    height = None
+    x = None
+    y = None
+    type = None # From template matching
--- traffic_sign_recognition.py
+++ traffic_sign_recognition.py
@@ -1,18 +1,24 @@
 import numpy as np
 import cv2
 
+import sign
+
 class TrafficSignRecognition():
+    signs = [] # of type Sign
+
     def __init__(self, frame):
         self.frame = frame
 
     def frame_preprocessing(self):
         self.hsv = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
         # define range of blue color in HSV
-        self.lower_blue = np.array([165, 0, 0])
-        self.upper_blue = np.array([195, 255, 255])
+        lower_blue = np.array([165, 0, 0])
+        upper_blue = np.array([195, 255, 255])
         # Threshold the HSV image to get only blue colors
-        self.mask = cv2.inRange(self.hsv, self.lower_blue, self.upper_blue)
+        mask = cv2.inRange(self.hsv, lower_blue, upper_blue)
         # Bitwise-AND mask and original image
-        self.res = cv2.bitwise_and(self.frame, self.frame, mask=self.mask)
+        self.after_mask = cv2.bitwise_and(self.frame, self.frame, mask=self.mask)
 
-        return self.res
+        return self.after_mask
+
+    def 
