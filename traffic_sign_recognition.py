504549965fbfb824b83023b66548eb5e471a7fae
Szymani <Szymanski996@gmail.com>
2020-06-15 20:02:18 +0100

    Binaryzation of images for recognition, in openCL

--- traffic_sign_recognition.py
+++ traffic_sign_recognition.py
@@ -14,6 +14,7 @@ class TrafficSignRecognition():
     def __init__(self):
         self.templates = []
         self.templates_hsv = []
+        self.templates_mask = []
         self.template_preprocesing()
 
 
@@ -24,6 +25,7 @@ class TrafficSignRecognition():
 
             h = template.shape[0]
             w = template.shape[1]
+
             # *Buffors
             template_buf = cl.image_from_array(GPUSetup.context, template, 4)
             fmt = cl.ImageFormat(cl.channel_order.RGBA, cl.channel_type.UNSIGNED_INT8)
@@ -34,7 +36,71 @@ class TrafficSignRecognition():
             template_hsv = np.empty_like(template)
             cl.enqueue_copy(GPUSetup.queue, template_hsv, dest_buf, origin=(0, 0), region=(w, h))
             self.templates_hsv.append(template_hsv)
+
+            # *Apply masks
+            template_mask = self.clear_sign(template_hsv, template)
+            self.templates_mask.append(template_mask)
     
+
+    def clear_sign(self,img_hsv, img=None):
+        h = img_hsv.shape[0]
+        w = img_hsv.shape[1]
+        cont_img_hsv = np.ascontiguousarray(img_hsv)
+
+        red_mask = np.zeros((1, 2), cl.cltypes.float4)
+        red_mask[0, 0] = (165, 90, 70, 0)  # Lower bound red
+        red_mask[0, 1] = (195, 255, 255, 0)  # Upper bound red
+        
+        black_mask = np.zeros((1, 2), cl.cltypes.float4)
+        black_mask[0, 0] = (0, 0, 0, 0)  # Lower bound black
+        black_mask[0, 1] = (255, 255, 90, 0)  # Upper bound black
+
+        #*Buffors
+        fmt = cl.ImageFormat(cl.channel_order.RGBA, cl.channel_type.UNSIGNED_INT8)
+        dest_buf = cl.Image(GPUSetup.context, cl.mem_flags.WRITE_ONLY, fmt, shape=(w, h))
+
+        #*Red mask
+        img_buf = cl.image_from_array(GPUSetup.context, cont_img_hsv, 4)
+        mask_buf = cl.Buffer(GPUSetup.context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=red_mask)
+        GPUSetup.program.hsv_bin_mask(GPUSetup.queue, (w, h), None, img_buf, mask_buf, dest_buf)
+        img_mask_red = np.empty_like(cont_img_hsv)
+        cl.enqueue_copy(GPUSetup.queue, img_mask_red, dest_buf, origin=(0, 0), region=(w, h))
+
+        #*Black mask
+        img_buf = cl.image_from_array(GPUSetup.context, cont_img_hsv, 4)
+        mask_buf = cl.Buffer(GPUSetup.context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=black_mask)
+        GPUSetup.program.hsv_bin_mask(GPUSetup.queue, (w, h), None, img_buf, mask_buf, dest_buf)
+        img_mask_black = np.empty_like(cont_img_hsv)
+        cl.enqueue_copy(GPUSetup.queue, img_mask_black, dest_buf, origin=(0, 0), region=(w, h))
+
+        #*Merge both 
+        img_buff_red = cl.image_from_array(GPUSetup.context, img_mask_red, 4)
+        img_buff_black = cl.image_from_array(GPUSetup.context, img_mask_black, 4)
+        GPUSetup.program.merge_bin(GPUSetup.queue, (w, h), None, img_buff_red, img_buff_black, dest_buf)
+        img_merge = np.empty_like(cont_img_hsv)
+        cl.enqueue_copy(GPUSetup.queue, img_merge, dest_buf, origin=(0, 0), region=(w, h))
+
+        # # *-----------------------------DEBUG---------------------------------
+        # img_concate_Verti = np.concatenate((img, img_hsv), axis=0)
+        # img_gray = img_hsv[:,:,2]
+        # blur = cv2.GaussianBlur(img_gray,(5,5),0)
+        # _, img_otsu = cv2.threshold(img_gray,0,255,cv2.THRESH_BINARY| cv2.THRESH_OTSU)
+
+        # cv2.imshow('Original and HSV', img_concate_Verti)
+        # cv2.imshow('Gray', img_gray)
+        # cv2.imshow("Otsu", img_otsu)
+        # cv2.imshow("After Red Mask",img_mask_red)
+        # cv2.imshow("After Black Mask",img_mask_black)
+        # cv2.imshow("Merge of masks", img_merge)
+
+        # key = cv2.waitKey(10)
+        # while (key != ord('w')):
+        #     key = cv2.waitKey(19)
+        #     pass
+        # # *-------------------------------------------------------------------
+
+        return img_merge
+
 
     def load_new_frame(self, frame):
         self.frame = cv2.cvtColor(frame, cv2.COLOR_RGB2RGBA)
@@ -67,7 +133,7 @@ class TrafficSignRecognition():
         # *Apply mask
         frame_buf = cl.image_from_array(GPUSetup.context, self.hsv, 4)
         mask_buf = cl.Buffer(GPUSetup.context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=mask)
-        GPUSetup.program.hsvMask(GPUSetup.queue, (w, h), None, frame_buf, mask_buf, dest_buf)
+        GPUSetup.program.hsv_mask(GPUSetup.queue, (w, h), None, frame_buf, mask_buf, dest_buf)
         self.after_mask = np.empty_like(frame)
         cl.enqueue_copy(GPUSetup.queue, self.after_mask, dest_buf, origin=(0, 0), region=(w, h))
         
@@ -166,27 +232,20 @@ class TrafficSignRecognition():
         print("***********************\nTemplates num: ", len(self.templates))
         print("Signs num: ", len(self.signs))
 
-        full_frame_img = self.hsv[:,:,2]
+        full_frame_img = self.hsv
         results = []
 
         start_time = time.time()
         for sign in self.signs:
-            # Load sign + Otsu's thresholding and Gaussian filtering
+            # Load sign + masking and binaryzation
             frame_img = full_frame_img[sign.y:sign.y+sign.height, sign.x:sign.x+sign.width]
-            blur = cv2.GaussianBlur(frame_img,(5,5),0)
-            _, frame_arr = cv2.threshold(frame_img,0,255,cv2.THRESH_BINARY| cv2.THRESH_OTSU)
-            # frame_arr = frame_img
+            frame_arr =  np.ascontiguousarray(self.clear_sign(frame_img))
 
             single_sign_results = []
-            for template in self.templates_hsv:
+            for template in self.templates_mask:
                 # Load template + Otsu's thresholding and Gaussian filtering for Template
-                template_img = cv2.resize(template, (sign.width, sign.height), interpolation=cv2.INTER_AREA)
-                template_img = template_img[:,:,2]
-
-                blur = cv2.GaussianBlur(template_img,(5,5),0)
-                _, template_arr = cv2.threshold(template_img,0,255,cv2.THRESH_BINARY| cv2.THRESH_OTSU)
-                # template_arr=template_img
-
+                template_arr = cv2.resize(template, (sign.width, sign.height), interpolation=cv2.INTER_AREA)
+                template_arr = np.ascontiguousarray(template_arr)
                 # # *-----------------------------DEBUG---------------------------------
                 img_concate_Verti = np.concatenate((frame_arr, template_arr), axis=0)
                 cv2.imshow('concatenated_Verti', img_concate_Verti)
