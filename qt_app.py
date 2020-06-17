fb48b869a024f4dfdc5f102d833fe346e025d675
amadeuszsz <amadeuszszymko@gmail.com>
2020-06-17 15:57:32 +0100

    Added button
    # Conflicts:
    #	main.py
    #	traffic_sign_recognition.py

--- qt_app.py
+++ qt_app.py
@@ -1,8 +1,26 @@
+'''
+Simple Qt gui for traffic signs recognition.
+Not working yet.
+'''
+
 import cv2
 import sys
-from PyQt5.QtWidgets import  QWidget, QLabel, QApplication
-from PyQt5.QtCore import QThread, Qt, pyqtSignal, pyqtSlot
+from PyQt5.QtWidgets import  QWidget, QLabel, QApplication, QDialog, QPushButton, QInputDialog, QFileDialog
+from PyQt5.QtCore import QThread, Qt, pyqtSignal, pyqtSlot, QDir, QUrl
 from PyQt5.QtGui import QImage, QPixmap
+import numpy as np
+import time
+import pyopencl as cl
+import pyopencl.cltypes
+import time
+import imutils
+import os
+
+from lane_detection import LaneDetection
+from traffic_sign_recognition import TrafficSignRecognition
+from GPUSetup import GPUSetup
+
+
 
 '''
 To display an OpenCV image, we have to convert the image 
@@ -14,16 +32,30 @@ class Thread(QThread):
     changePixmap = pyqtSignal(QImage)
 
     def run(self):
-        cap = cv2.VideoCapture("videos/video1.mp4")
-        while True:
-            ret, frame = cap.read()
-            if ret:
-                rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                h, w, ch = rgbImage.shape
-                bytesPerLine = ch * w
-                convertToQtFormat = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
-                p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
-                self.changePixmap.emit(p)
+        signRecognizer = TrafficSignRecognition()
+        name = "Traffic Sign Recognition"
+        self.filepath = ""
+        cap = cv2.VideoCapture(self.filepath)
+        if self.filepath:
+            while True:
+                ret, frame = cap.read()
+                if ret:
+                    ret, frame = cap.read()
+                    signRecognizer.load_new_frame(frame)
+                    original_frame, marked_frame = signRecognizer.connected_components()
+                    final_frame = np.concatenate((original_frame, marked_frame), axis=0)
+                    rgbImage = cv2.cvtColor(final_frame, cv2.COLOR_BGR2RGB)
+                    h, w, ch = rgbImage.shape
+                    bytesPerLine = ch * w
+                    convertToQtFormat = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
+                    p = convertToQtFormat.scaled(768, 864, Qt.KeepAspectRatio)
+                    self.changePixmap.emit(p)
+                    key = cv2.waitKey(1)
+                    if key == ord('q'):
+                        break
+                    if key == ord('w'):
+                        cv2.waitKey(-1)
+
 
 class App(QWidget):
     def __init__(self):
@@ -31,27 +63,36 @@ class App(QWidget):
         self.title = 'PyQt5 Video'
         self.left = 100
         self.top = 100
-        self.width = 640
-        self.height = 480
+        self.width = 768 + 2*20 + 500
+        self.height = 864 + 2*20
         self.initUI()
 
     @pyqtSlot(QImage)
     def setImage(self, image):
         self.label.setPixmap(QPixmap.fromImage(image))
 
+    @pyqtSlot()
+    def import_video(self):
+        #TODO
+        self.filepath = QFileDialog.getOpenFileUrl(self, "Open file", directory=QUrl(os.path.abspath(os.path.dirname(__file__))),
+                                                   filter="Video files (*.mp4)")
+
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        self.resize(1800, 1200)
-        # create a label
+        # Create a button
+        self.import_video_button = QPushButton("Import video", self)
+        self.import_video_button.move(808, 20)
+        self.import_video_button.clicked.connect(self.import_video)
+        # Create a video label
         self.label = QLabel(self)
-        self.label.move(280, 120)
-        self.label.resize(640, 480)
+        self.label.move(20, 20)
+        self.label.resize(768, 864)
         th = Thread(self)
         th.changePixmap.connect(self.setImage)
         th.start()
-
         self.show()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
